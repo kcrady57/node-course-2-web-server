a05c6b4b0aefc30a018fe5942bba77685ef2cea8
Kaye Crady <kcrady57@gmail.com>
2017-08-06 14:55:02 -0600

    Added new page projects.hbs

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+       pageTitle: 'Projects Page',
+    });
+});
+
 app.get('/maintenance', (req, res) => {
     res.render('maintenance.hbs');
 });
